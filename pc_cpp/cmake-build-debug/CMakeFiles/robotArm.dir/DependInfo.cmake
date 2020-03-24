c5df2102a74bb912821faee52cdf1a1b777ea1fd
bb1950328 <bb1950328@gmail.com>
2020-03-24 19:32:32 +0100

    some intellij files...

--- pc_cpp/cmake-build-debug/CMakeFiles/robotArm.dir/DependInfo.cmake
+++ pc_cpp/cmake-build-debug/CMakeFiles/robotArm.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/bab21/CLionProjects/robotArm/main.cpp" "/home/bab21/CLionProjects/robotArm/cmake-build-debug/CMakeFiles/robotArm.dir/main.cpp.o"
+  "/home/bab21/PycharmProjects/robotArm/pc_cpp/main.cpp" "/home/bab21/PycharmProjects/robotArm/pc_cpp/cmake-build-debug/CMakeFiles/robotArm.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- pc_cpp/cmake-build-debug/cmake_install.cmake
+++ pc_cpp/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/bab21/CLionProjects/robotArm
+# Install script for directory: /home/bab21/PycharmProjects/robotArm/pc_cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/bab21/CLionProjects/robotArm/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/bab21/PycharmProjects/robotArm/pc_cpp/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
