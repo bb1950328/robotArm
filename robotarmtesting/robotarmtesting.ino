5aed0028aca195b1c1e5cf741ee82b0f5e4a6f42
Jan Kläger <info@janklaeger.ch>
2020-04-22 13:06:37 +0200

    added speed and commented the code

--- robotarmtesting/robotarmtesting.ino
+++ robotarmtesting/robotarmtesting.ino
@@ -28,16 +28,20 @@ int gripperTurnAngle = 90;
 int gripperOpenAngle = 90;
 
 void setup() {
+    // Servos den Pins zuweisen
     sa.attach(5);
     sb.attach(10);
     table.attach(3);
     gripperLower.attach(9);
     gripperTurn.attach(6);
     gripperOpen.attach(11);
+
+    // Nunchuk einbinden
     Serial.begin(9600);
     Wire.begin();
     nunchuk_init();
-  
+
+    // Buttons als Inputs definieren
     pinMode(BUTTON_OPEN_GRIPPER_PIN, INPUT_PULLUP);
     pinMode(BUTTON_CLOSE_GRIPPER_PIN, INPUT_PULLUP);
     pinMode(BUTTON_TURN_LEFT_PIN, INPUT_PULLUP);
@@ -48,28 +52,48 @@ void setup() {
 
 void loop() {
   if (nunchuk_read()) {
+    // Werte von Joystick auslesen
     float x = nunchuk_joystickX();
     float y = nunchuk_joystickY();
     int c = nunchuk_buttonC();
     int z = nunchuk_buttonZ();
 
 
-    if(x > 90 && MIN < tableW) {
-      tableW -= 1;
-    }
-  
-    if(x < -90 && MAX > tableW) {
-      tableW += 1;
-    }
-  
-    if(y > 90 && MIN < angleB) {
-      angleB -= 1;
-    }
-  
-    if(y < -90 && MAX > angleB) {
-      angleB += 1;
+    // Roboter drehen
+    if(MIN < tableW) {
+      if(x > 90) {
+        tableW -= 1;
+      } else if(x > 30) {
+        tableW -= 0.5;
+      }
     }
 
+    if(MAX > tableW) {
+      if(x < -90) {
+        tableW += 1;
+      } else if(x < -30) {
+        tableW += 0.5;
+      }
+    }
+
+    // Roboterarm vorne bewegen
+    if(MIN < angleB) {
+      if(y > 90) {
+        angleB -= 1;
+      } else if(y > 30) {
+        angleB -= 0.5;
+      }
+    }
+
+    if(MAX > angleB) {
+      if(y < -90) {
+        angleB += 1;
+      } else if(y < -30) {
+        angleB += 0.5;
+      }
+    }
+
+    // Roboterarm hinten bewegen
     if(z == 1 && MIN < angleA) {
       angleA -= 1;
     }
@@ -78,26 +102,28 @@ void loop() {
       angleA += 1;
     }
 
+    // Greifer nach oben oder unten
     if (!digitalRead(BUTTON_ANGLE_HIGHER_PIN) && MAX > gripperLowerAngle) {
         gripperLowerAngle += 1;
     } else if (!digitalRead(BUTTON_ANGLE_LOWER_PIN) && MIN < gripperLowerAngle) {
         gripperLowerAngle -= 1;
     }
-    
+
+    // Greifer drehen
     if (!digitalRead(BUTTON_TURN_LEFT_PIN) && MAX > gripperTurnAngle) {
         gripperTurnAngle += 1;
     } else if (!digitalRead(BUTTON_TURN_RIGHT_PIN) && MIN < gripperTurnAngle) {
         gripperTurnAngle -= 1;
     }
-    
+
+    // greifen
     if (!digitalRead(BUTTON_OPEN_GRIPPER_PIN) && MAX > gripperOpenAngle) {
         gripperOpenAngle += 1;
     } else if (!digitalRead(BUTTON_CLOSE_GRIPPER_PIN) && MIN < gripperOpenAngle) {
         gripperOpenAngle -= 1;
     }
     
-    
-    
+    // alle Servos bewegen
     sa.write(angleA);
     sb.write(angleB);
     table.write(tableW);
